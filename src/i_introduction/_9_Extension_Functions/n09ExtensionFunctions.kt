9a60d845c8c6da6474567cf9b78d25526ce03786
Sahil Muthoo <sahil.muthoo@gmail.com>
2018-05-06 13:48:46 +0100

    Implement extension funcs

--- src/i_introduction/_9_Extension_Functions/n09ExtensionFunctions.kt
+++ src/i_introduction/_9_Extension_Functions/n09ExtensionFunctions.kt
@@ -4,7 +4,7 @@ import util.TODO
 import util.doc9
 
 // declares an extension function that returns the last character
-fun String.lastChar() = this.get(this.length - 1)
+fun String.lastChar() = this[this.length - 1]
 
 
 // 'this' refers to the receiver (String) and can be omitted
@@ -29,7 +29,7 @@ fun todoTask9(): Nothing = TODO(
 
 data class RationalNumber(val numerator: Int, val denominator: Int)
 
-fun Int.r(): RationalNumber = todoTask9()
-fun Pair<Int, Int>.r(): RationalNumber = todoTask9()
+fun Int.r(): RationalNumber = RationalNumber(this, 1)
+fun Pair<Int, Int>.r(): RationalNumber = RationalNumber(this.first, this.second)
 
 
