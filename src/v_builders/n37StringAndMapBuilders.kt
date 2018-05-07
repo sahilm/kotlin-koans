5f574b7f7e2eebc1a269105e225611daea37f288
Sahil Muthoo <sahil.muthoo@gmail.com>
2018-05-07 23:23:40 +0100

    Builders done.

--- src/v_builders/n37StringAndMapBuilders.kt
+++ src/v_builders/n37StringAndMapBuilders.kt
@@ -1,7 +1,6 @@
 package v_builders
 
 import util.TODO
-import java.util.*
 
 fun buildStringExample(): String {
     fun buildString(build: StringBuilder.() -> Unit): String {
@@ -29,11 +28,16 @@ fun todoTask37(): Nothing = TODO(
 )
 
 fun task37(): Map<Int, String> {
-    todoTask37()
-//    return buildMap {
-//        put(0, "0")
-//        for (i in 1..10) {
-//            put(i, "$i")
-//        }
-//    }
+    return buildMap {
+        put(0, "0")
+        for (i in 1..10) {
+            put(i, "$i")
+        }
+    }
+}
+
+fun <K, V> buildMap(build: MutableMap<K, V>.() -> Unit): Map<K, V> {
+    val hashMap = HashMap<K, V>()
+    hashMap.build()
+    return hashMap
 }
--- src/v_builders/n39HtmlBuilders.kt
+++ src/v_builders/n39HtmlBuilders.kt
@@ -6,7 +6,7 @@ import v_builders.data.getProducts
 import v_builders.htmlLibrary.*
 
 fun getTitleColor() = "#b9c9fe"
-fun getCellColor(row: Int, column: Int) = if ((row + column) %2 == 0) "#dce4ff" else "#eff2ff"
+fun getCellColor(row: Int, column: Int) = if ((row + column) % 2 == 0) "#dce4ff" else "#eff2ff"
 
 fun todoTask39(): Nothing = TODO(
     """
@@ -22,7 +22,7 @@ fun todoTask39(): Nothing = TODO(
 fun renderProductTable(): String {
     return html {
         table {
-            tr {
+            tr(getTitleColor()) {
                 td {
                     text("Product")
                 }
@@ -34,7 +34,19 @@ fun renderProductTable(): String {
                 }
             }
             val products = getProducts()
-            todoTask39()
+            products.forEachIndexed { index, product ->
+                tr {
+                    td(getCellColor(index, 1)) {
+                        text(product.description)
+                    }
+                    td(getCellColor(index, 2)) {
+                        text(product.price)
+                    }
+                    td(getCellColor(index, 3)) {
+                        text(product.popularity)
+                    }
+                }
+            }
         }
     }.toString()
 }
