148d95f7ea5087a62f508213c4e08742a62bde02
Sahil Muthoo <sahil.muthoo@gmail.com>
2018-05-07 14:03:07 +0100

    Foo.

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,8 +1,16 @@
 package iii_conventions
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
+    override fun compareTo(other: MyDate): Int {
+        return when {
+            year != other.year -> year - other.year
+            month != other.month -> month - other.month
+            else -> dayOfMonth - other.dayOfMonth
+        }
+    }
+}
 
-operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
+operator fun MyDate.rangeTo(other: MyDate): DateRange = DateRange(this, other)
 
 enum class TimeInterval {
     DAY,
@@ -10,4 +18,23 @@ enum class TimeInterval {
     YEAR
 }
 
-class DateRange(val start: MyDate, val endInclusive: MyDate)
+class DateRange(val start: MyDate, val endInclusive: MyDate) {
+    operator fun contains(date: MyDate): Boolean {
+        return date >= start && date <= endInclusive
+    }
+
+    operator fun iterator(): Iterator<MyDate> {
+        return DateIterator(this)
+    }
+}
+
+class DateIterator(private val dateRange: DateRange) : Iterator<MyDate> {
+    var current = dateRange.start
+    override fun hasNext() = current <= dateRange.endInclusive
+
+    override fun next(): MyDate {
+        val result = current
+        current = current.nextDay()
+        return result
+    }
+}
